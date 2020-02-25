2ee14f5ce3648958ae20dd6127ebc9a3acbb4dd1
Penta <preuvinou@gmail.com>
2020-02-25 18:08:07 +0100

    Add project files.

---
+++ FFmpeg/Commands/ToolbarCommands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace FFmpeg.Commands
+{
+    static class ToolbarCommands
+    {
+        public static RoutedUICommand Quit { get; } = new RoutedUICommand("Quit", "Quit", typeof(ToolbarCommands));
+        public static RoutedUICommand Minimize { get; } = new RoutedUICommand("Minimize", "Minimize", typeof(ToolbarCommands));
+    }
+}
---
+++ FFmpeg/Toolbar.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FFmpeg
+{
+    /// <summary>
+    /// Interaction logic for Toolbar.xaml
+    /// </summary>
+    public partial class Toolbar : UserControl
+    {
+        public Toolbar()
+        {
+            InitializeComponent();
+        }
+
+        private void QuitCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void QuitCommandExecuted(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = true;
+    
+        private void MinimizeCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+
+        private void MinimizeCommandExecuted(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = true;
+    }
+}
